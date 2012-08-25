56fe342acd12845d3622a1026c72714463648787
Liam S. Crouch <petterroea@skymiastudios.com>
2012-08-25 09:56:35 +0200

    Added applet class

---
+++ src/Game.java
@@ -0,0 +1,23 @@
+import java.applet.Applet;
+
+
+public class Game extends Applet{
+	public static void main(String[] args)
+	{
+		
+	}
+	@Override
+	public void init()
+	{
+		
+	}
+	public void start()
+	{
+		
+	}
+	@Override
+	public void stop()
+	{
+		
+	}
+}
